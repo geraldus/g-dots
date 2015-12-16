380f3c0963dbab18dcab4a1e9e835dccb5b6046a
Arthur Fayzrakhmanov (Артур Файзрахманов) <heraldhoi@gmail.com>
2015-12-16 06:27:55 +0500

    Initial commit (from NixOS)

---
+++ .emacs.d/g/autopack.el
@@ -0,0 +1,38 @@
+;;; autopack --- Automatically install neccessary packages from MELPA.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl-lib)
+
+(defvar g/packages
+  '(company
+    color-theme-sanityinc-tomorrow
+    diff-hl
+    dockerfile-mode
+    exec-path-from-shell
+    expand-region
+    fill-column-indicator
+    flatui-theme
+    flx-ido
+    flycheck
+    flycheck-haskell
+    haskell-mode
+    magit
+    markdown-mode
+    multiple-cursors
+    smex
+    shakespeare-mode
+    undo-tree
+    yaml-mode)
+  "List of packages to be installed from archives.")
+
+(defun g/packages-installed-p ()
+  "Determine if all packages are installed."
+  (cl-loop for pkg in g/packages
+           when (not (package-installed-p pkg)) do (cl-return nil)
+           finally (cl-return t)))
+
+(provide 'autopack)
+;;; autopack.el ends here
